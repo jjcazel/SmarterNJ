b41f7ac8f738dd6e5a21ce01ee596eb74c37e2f3
Jared Cazel <jjcazel@gmail.com>
2024-01-14 18:05:52 -0500

    Update the links to navlinks with react router to work with the hash router

--- src/components/SubNavigation.tsx
+++ src/components/SubNavigation.tsx
@@ -1,28 +1,9 @@
-import { useEffect } from "react";
+import { NavLink } from "react-router-dom";
 
 import "../styles/SubNavigation.css";
 import NJLogo from "/NJLogo.png";
 
 export function SubNavigation() {
-  useEffect(() => {
-    // Get the current pathname
-    const currentPath = window.location.pathname;
-    // Get all anchor tags with the class 'navLink'
-    const links = document.querySelectorAll(".navLink");
-    // Loop through the links and add a class if the href matches the current path
-    links.forEach((link) => {
-      if (link.getAttribute("href") === currentPath) {
-        link.classList.add("activeLink");
-      }
-    });
-    // Cleanup: Remove the 'activeLink' class when the component unmounts
-    return () => {
-      links.forEach((link) => {
-        link.classList.remove("activeLink");
-      });
-    };
-  }, []);
-
   return (
     <nav>
       <div className="subNav-left">
@@ -32,12 +13,12 @@ export function SubNavigation() {
       <div className="mobileLinksContainer">
         {" "}
         <div className="linksContainer">
-          <a href="#/" className="navLink">
+          <NavLink to="/" className="navLink" end>
             Home
-          </a>
-          <a href="/#news" className="navLink">
+          </NavLink>
+          <NavLink to="/news" className="navLink">
             News
-          </a>
+          </NavLink>
           <a
             href="https://smarter.nj.gov/#contact"
             target="_blank"
